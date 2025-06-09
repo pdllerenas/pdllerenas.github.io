13c84882a72ad6c71d31b8092ad7b719779f7f33
pdllerenas <pdllerenas@gmail.com>
2025-06-09 16:51:16 -0600

    removed some links

---
+++ script.js
@@ -0,0 +1,15 @@
+const buttons = document.querySelectorAll('[data-carousel-button]');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const offset = button.dataset.carouselButton === 'next' ? 1 : -1;
+    const slides = button
+      .closest('[data-carousel]')
+      .querySelector('[data-slides]');
+    const activeSlide = slides.querySelector('[data-active]');
+    let newIndex = ([...slides.children].indexOf(activeSlide) + offset + slides.children.length) % slides.children.length;
+
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+  });
+});
